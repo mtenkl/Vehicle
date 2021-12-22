7c89201daf2e51476c2b88f7eda3356aadca5a25
Michal Tenkl <michaltenkl@gmail.com>
2021-12-22 21:17:03 +0100

    PID controller for brakes and throttle

--- drivermodel.py
+++ drivermodel.py
@@ -3,68 +3,108 @@ import vehiclemodel3dof
 import matplotlib.pyplot as plt
 
 
+class DriverModel(object):
 
-vehicle = vehiclemodel3dof.VehicleDynamicModel3dof("mazda.ini")
+    SLOW_DRIVER = 1
+    NORMAL_DRIVER = 0
+    FAST_DRIVER = 2
+
+    def __init__(self, driver_type=NORMAL_DRIVER) -> None:
+        super().__init__()
+        self._vehicle = vehiclemodel3dof.VehicleDynamicModel3dof("mazda.ini")
+        self._speed_histeresis = 1
+        self._dt = 0.1
+        self._driver_type = driver_type
+        self._throttle_levels = [80, 50, 100]
+
+        self.vehicle_speeds = list()
+        self.target_speed = list()
+
+        self.times = list()
+        self.throttle = list()
+        self.brake = list()
+        self.time = 0
+
+    def wait(self, time):
+
+        t0 = self.time
+
+        while True:
+
+            self._vehicle.update(self._dt)
+            self.vehicle_speeds.append(self._vehicle.vehicle_speed_kmph)
+            self.throttle.append(self._vehicle.throttle_pedal)
+
+            self.time += self._dt
+            self.times.append(self.time)
+
+            if self.time - t0 >= time:
+                break
 
 
-target_speed = 170
-speed_hist = 5
 
-dt = 0.1
-time = np.linspace(0, 100, 1001)
-speed = list()
-throttle = list()
-error = list()
-target_v = list()
-traction = list()
+    def drive(self, time, speed, speed_profile=None):
+
+        K_P = 10
+        K_I = 4
+        K_D = 1
+
+        K_Pb = -2
+        K_Ib = -40
+        K_Db = -1
+
+        # abs(target_speed - self._vehicle.vehicle_speed_kmph) > self._speed_histeresis:
+        while self.time < time:
+
+            if speed_profile is not None:
+                if round(self.time) in speed_profile:
+                    speed = speed_profile[round(self.time)]
+
+            error = speed - self._vehicle.vehicle_speed_kmph
+
+            prop = K_P * error
+            integ = K_I * error * self._dt
+            der = K_D * error / self._dt
+
+            prop_b = K_Pb * error
+            integ_b = K_Ib * error * self._dt
+            der_b = K_Db * error / self._dt
+
+            output = prop + integ + der
+            output_b = prop_b + integ_b + der_b
+
+            self._vehicle.throttle_pedal = np.clip(
+                output, 0, self._throttle_levels[self._driver_type])
+
+            self._vehicle.brake_pedal = np.clip(
+                output_b, 0, self._throttle_levels[self._driver_type])
+
+            self._vehicle.update(self._dt)
+
+            self.vehicle_speeds.append(self._vehicle.vehicle_speed_kmph)
+            self.target_speed.append(speed)
+            self.throttle.append(self._vehicle.throttle_pedal)
+            self.brake.append(self._vehicle.brake_pedal)
+
+            self.time += self._dt
+            self.times.append(self.time)
 
 
-kp = 10
-ki = 1
-kd = 1
 
-for t in time:
+if __name__ == "__main__":
 
-    """if vehicle.vehicle_speed_kmph < target_speed - speed_hist:
-        vehicle.throttle = 100
-    else:
-        vehicle.throttle = 0"""
+    driver = DriverModel()
 
-    if t > 20:
-        target_speed = 100
-    if t > 40:
-        target_speed = 50
-    if t > 60:
-        target_speed = 130
+    target_speed = 50
+    speed_profile = {0: 100, 10: 40, 20: 60}
 
-    e = target_speed - vehicle.vehicle_speed_kmph
+    driver.drive(40, target_speed, speed_profile)
+    
 
-    prop = kp * e
-    integ = ki * e * dt
-    der = kd * e / dt
+    plt.plot(driver.times, driver.vehicle_speeds, "-", linewidth=4)
+    plt.plot(driver.times, driver.throttle)
+    plt.plot(driver.times, driver.brake)
+    plt.plot(driver.times, driver.target_speed, "--r")
+    plt.legend(["Speed", "Throttle", "Brake", "Target speed"])
 
-    output = prop + integ + der
-    output = 100
-    if t > 20:
-        output = 0
-
-    vehicle.throttle_pedal = np.clip(output, 0, 100)
-
-    vehicle.update(0.1)
-    speed.append(vehicle.vehicle_speed_kmph)
-    throttle.append(vehicle.throttle_pedal)
-    traction.append(vehicle._longitudinal_force)
-    error.append(e)
-    target_v.append(target_speed)
-
-plt.subplot(2, 3, 1)
-plt.plot(time, speed)
-plt.subplot(2, 3, 2)
-plt.plot(time, throttle)
-plt.subplot(2, 3, 3)
-plt.plot(time, target_v)
-plt.subplot(2, 3, 4)
-plt.plot(time, traction)
-plt.legend()
-
-plt.show()
+    plt.show()
