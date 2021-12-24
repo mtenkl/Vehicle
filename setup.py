863c8e51c1328419d5660a804046835d0edcc416
Michal Tenkl <michaltenkl@gmail.com>
2021-12-24 16:41:37 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='vehicle',
+   version='1.0',
+   description='A useful module',
+   author='Michal Tenkl',
+   author_email='',
+   packages=['vehicle'],  #same as name
+   install_requires=['numpy', 'matplotlib'], #external packages as dependencies
+)
